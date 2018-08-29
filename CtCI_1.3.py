b5e6eea3a0303008bbb5b85b98f13dd68bb5195f
Amanda Cameron <amanda.cameron103@gmail.com>
2018-08-29 12:09:23 -0700

    initial commit

---
+++ CtCI_1.3.py
@@ -0,0 +1,23 @@
+# URLify
+# write a method to replace all spaces in string with "%20"
+
+def URLify(s):
+    URL_s = s
+
+    URL_list = URL_s.split()
+
+    char = "%20"
+    return char.join(URL_list)
+
+
+print(URLify("amanda is the best"))
+
+sec = "what is happening here"
+
+print(sec)
+
+print(URLify(sec))
+
+print(sec)
+
+C
